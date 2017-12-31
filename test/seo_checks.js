26d96411798332a7a0e5e39b66e9e9db1b9e432d
cuuboy <yuzhe.c85@gmail.com>
2017-12-31 14:28:23 +0800

    Img tag check with test case

--- test/seo_checks.js
+++ test/seo_checks.js
@@ -1,21 +1,22 @@
 const should = require('should')
 const checks = require('../lib/seo_checks')
-describe('#average', () => {
-  it('should return the average of array', done => {
-    var avg = checks.average([1, 2, 3, 4])
-    avg.should.equal(2.5)
-    done()
-  })
-  it('should return NaN when array is empty', done => {
-    var avg = checks.average([])
-    isNaN(avg).should.be.true
-    done()
-  })
-})
 
 describe('#checkImg', () => {
-  it('should return number of image tags without alt attribute', done => {
-    var doc = '<img src="localhost/test.jpg" /><img src="localhost/test.jpg" />'
-    checks.checkImg(doc).should.equal("There are 2 <img> tag without alt attribute")
+  it('returns number of image tags without alt attribute', done => {
+    var doc = '<img src="localhost/test.jpg" /><img src="localhost/test.jpg" />';
+    checks.checkImg(doc).should.equal("There are 2 <img> tag without alt attribute");
+    done();
+  })
+
+  it('returns correct number when some of img have alt attribute', done => {
+    var doc = '<img src="localhost/test.jpg" /><img alt="valid" src="localhost/test.jpg" />';
+    checks.checkImg(doc).should.equal("There are 1 <img> tag without alt attribute");
+    done();
+  })
+
+  it('returns null when all img tags contain alt attribute', done => {
+    var doc = '<img alt="valid" src="localhost/test.jpg" /><img alt="valid" src="localhost/test.jpg" />';
+    should(checks.checkImg(doc)).equal(null);
+    done();
   })
 })
