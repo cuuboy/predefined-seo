6c7540b111dadcf30555e34f97c6cac6843b4f37
cuuboy <yuzhe.c85@gmail.com>
2018-01-01 22:31:16 +0800

    Add index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const checks = require('lib/seo_checks.js');
+module.exports = {
+    checkHtmlFile: function(filePath, rules) {
+        return checks.checkHtmlFile(filePath, rules);
+    },
+    checkReadableStream: function(rs, rules) {
+        return checks.checkReadableStream(rs, rules);
+    }
+}
